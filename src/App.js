7260316702bfead263fa8d8ebc9b5f265277f7de
kadowning1 <kadowning1@gmail.com>
2021-10-25 16:40:05 -0400

    sandbox

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+// import axios from "axios";
+
+
+// const axios = require('axios');
+
+import React from 'react';
+import './App.css';
+import Dashboard from '../Components/Dashboard';
+import LogIn from '../Components/LogIn';
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/login">
+            <LogIn />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+// getAPI() {
+//  const apiKey = "https://port-3000-aincbootcampapi-ianrios529550.codeanyapp.com"
+// // Make a request for a user with a given ID
+// axios.get(apiKey)
+//   .then(function (response) {
+//     // handle success
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   })
+//   .then(function () {
+//     // always executed
+//   });
+// }
